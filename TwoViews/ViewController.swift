095357bbd8f336f664037300312d664610bddf88
komplektan <54449303+komplektan@users.noreply.github.com>
2022-03-11 20:35:29 +0500

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by Вячеслав Квашнин on 11.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
